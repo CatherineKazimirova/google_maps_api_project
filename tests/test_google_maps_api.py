e2456188b250938d4041d19112e28d1998592597
Catherine Kazimirova <catherine.cazimirova@yandex.ru>
2024-05-20 00:09:41 +0300

    added allure steps, logging + function in test_google_maps_api.py was divided into six functions

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -2,45 +2,54 @@ from utilities.api_requests import GoogleMapsApi
 from utilities.base_methods import BaseMethods
 from utilities.validation_schemes import Validation
 from faker import Faker
+import allure
 
 faker = Faker()
 
 
+@allure.epic('Create/get/update/delete location')
 class TestLocation:
+    place_id = ''
+    address = faker.address()
+    new_address = faker.address()
 
-    def test_create_and_check_location(self):
+    @allure.description('Create location')
+    @staticmethod
+    def test_create_location():
         """Создание локации и проверка ответа"""
         print('CREATING LOCATION')
-        """Генерация адреса через Faker"""
-        address = faker.address()
         """Создание переменной со схемой валидации"""
         post_response_schema = Validation.post_response()
         """Запрос на создание локации, в address передается сгенерированный Faker адрес"""
-        response_post = GoogleMapsApi.create_location(address)
+        response_post = GoogleMapsApi.create_location(TestLocation.address)
         """Сохранение ответа на запрос в переменную"""
         response_post_json = response_post.json()
         """Сохранение place_id в переменную"""
-        place_id = response_post_json.get('place_id')
+        TestLocation.place_id = response_post_json.get('place_id')
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('POST', 200, response_post)
         BaseMethods.check_response('POST', response_post_json, post_response_schema)
         print('LOCATION CREATED', '\n')
 
+    @allure.description('Get created location')
+    @staticmethod
+    def test_get_location():
         print('GETTING CREATED LOCATION')
         """Получение локации и проверка ответа"""
         """Создание переменной со схемой валидации"""
-        get_response_schema = Validation.get_response(address)
+        get_response_schema = Validation.get_response(TestLocation.address)
         """Запрос на получение локации, в place_id передается сохраненный из ответа на запрос POST id"""
-        response_get = GoogleMapsApi.get_location(place_id)
+        response_get = GoogleMapsApi.get_location(TestLocation.place_id)
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('GET', 200, response_get)
         BaseMethods.check_response('GET', response_get.json(), get_response_schema)
         print('CREATED LOCATION RECEIVED', '\n')
 
+    @allure.description('Update created location')
+    @staticmethod
+    def test_update_location():
         print('UPDATING CREATED LOCATION')
         """Обновление локации и проверка ответа"""
-        """Генерация нового адреса через Faker"""
-        new_address = faker.address()
         """Создание переменных с ожидаемыми названием поля и значением в этом поле"""
         put_response_field_name, put_response_message = "msg", "Address successfully updated"
         """Создание переменной со схемой валидации, в переменную передаются ожидаемое название поля
@@ -48,23 +57,29 @@ class TestLocation:
         put_response_schema = Validation.standard_response(put_response_field_name, put_response_message)
         """Запрос на обновление локации, в place_id передается сохраненный из ответа на запрос POST id,
         в new_address - новый сгенерированный Faker адрес"""
-        response_put = GoogleMapsApi.update_location(place_id, new_address)
+        response_put = GoogleMapsApi.update_location(TestLocation.place_id, TestLocation.new_address)
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('PUT', 200, response_put)
         BaseMethods.check_response('PUT', response_put.json(), put_response_schema)
         print('LOCATION UPDATED', '\n')
 
+    @allure.description('Get updated location')
+    @staticmethod
+    def test_get_updated_location():
         print('GETTING UPDATED LOCATION')
         """Получение обновленной локации и проверка ответа"""
         """Создание переменной со схемой валидации, в схему передается новый адрес"""
-        get_response_schema = Validation.get_response(new_address)
+        get_response_schema = Validation.get_response(TestLocation.new_address)
         """Запрос на получение локации, в place_id передается сохраненный из ответа на запрос POST id"""
-        response_get = GoogleMapsApi.get_location(place_id)
+        response_get = GoogleMapsApi.get_location(TestLocation.place_id)
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('GET', 200, response_get)
         BaseMethods.check_response('GET', response_get.json(), get_response_schema)
         print('UPDATED LOCATION RECEIVED', '\n')
 
+    @allure.description('Delete location')
+    @staticmethod
+    def test_delete_created_and_updated_location():
         print('DELETING LOCATION')
         """Получение созданной и обновленной локации и проверка ответа"""
         """Создание переменных с ожидаемыми названием поля и значением в этом поле"""
@@ -73,12 +88,15 @@ class TestLocation:
         и значение в этом поле"""
         delete_response_schema = Validation.standard_response(delete_response_field_name, delete_response_message)
         """Запрос на удаление локации, в place_id передается сохраненный из ответа на запрос POST id"""
-        response_delete = GoogleMapsApi.delete_location(place_id)
+        response_delete = GoogleMapsApi.delete_location(TestLocation.place_id)
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('DELETE', 200, response_delete)
         BaseMethods.check_response('DELETE', response_delete.json(), delete_response_schema)
         print('LOCATION DELETED', '\n')
 
+    @allure.description('Get deleted location')
+    @staticmethod
+    def test_get_deleted_location():
         print('GETTING DELETED LOCATION')
         """Попытка получения удаленной локации и проверка ответа"""
         """Создание переменных с ожидаемыми названием поля и значением в этом поле"""
@@ -87,7 +105,7 @@ class TestLocation:
         """Создание переменной со схемой валидации, в переменную передаются ожидаемое название поля
         и значение в этом поле"""
         get_response_schema = Validation.standard_response(get_response_field_name, get_response_message)
-        response_get = GoogleMapsApi.get_location(place_id)
+        response_get = GoogleMapsApi.get_location(TestLocation.place_id)
         """Проверка статус-кода и проверка ответа валидатором"""
         BaseMethods.check_status_code('GET', 404, response_get)
         BaseMethods.check_response('GET', response_get.json(), get_response_schema)
--- utilities/http_methods.py
+++ utilities/http_methods.py
@@ -1,4 +1,6 @@
+import allure
 import requests
+from utilities.logger import Logger
 
 
 class HttpMethods:
@@ -7,21 +9,33 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
-        result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
-        return result
+        with allure.step('GET'):
+            Logger.add_request(url, 'GET')
+            result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def post(url, body):
-        result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
-        return result
+        with allure.step('POST'):
+            Logger.add_request(url, 'POST')
+            result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def put(url, body):
-        result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
-        return result
+        with allure.step('PUT'):
+            Logger.add_request(url, 'PUT')
+            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def delete(url, body):
-        result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
-        return result
+        with allure.step('DELETE'):
+            Logger.add_request(url, 'DELETE')
+            result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookies)
+            Logger.add_response(result)
+            return result
     
